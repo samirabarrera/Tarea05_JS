f76947e87dc8964c87e46f9dac38518d7c2fe4bd
Samira Barrera <164458958+samirabarrera@users.noreply.github.com>
2024-05-29 19:23:06 -0600

    Updated Problema1.js

---
+++ Tarea 5JS/Problema1.js
@@ -0,0 +1,130 @@
+/*¿Puedes mostrar estas características de los
+usuarios de manera ordenada y coherente en consola? ¿Tienen los
+usuarios alguna funcionalidad, pueden hacer algo?*/
+
+//usuario0
+let nombre = 'Paola';
+let apellido = 'Ortiz';
+let email = 'paola@company.ru'
+let direccion = {
+    municipio: 'Guatemala',
+    calle: 'Calle Principal',
+    numero: 25,
+}
+
+//usuario1
+let nombre1 = 'Danilo';
+let apellido1 = 'Rodriguez';
+let email1 = 'danilo@company.ru'
+let direccion1 = {
+    municipio: 'Guatemala',
+    calle: 'Calle Ancha',
+    numero: 26,
+}
+
+//usuario2
+let nombre2 = 'Pablo';
+let apellido2 = 'Perez';
+let email2 = 'pablo@company.ru'
+let direccion2 = {
+    municipio: 'Guatemala',
+    calle: 'Calle Angosta',
+    numero: 27,
+}
+
+//usuario3
+let nombre3 = 'Andrea';
+let apellido3 = 'González';
+let email3 = 'andrea@company.ru'
+let direccion3 = {
+    municipio: 'Guatemala',
+    calle: 'Última calle',
+    numero: 28,
+}
+
+//usuario4
+let nombre4 = 'Marta';
+let apellido4 = 'Pérez';
+let email4 = 'marta@company.ru'
+let direccion4 = {
+    municipio: 'Guatemala',
+    calle: 'Calle Boulevard',
+    numero: 29,
+}
+
+//usuario5
+let nombre5 = 'Daniela';
+let apellido5 = 'Mendez';
+let email5 = 'daniela@company.ru'
+let direccion5 = {
+    municipio: 'Guatemala',
+    calle: 'Calle Fondo',
+    numero: 30,
+}
+
+//FORMA ORDENADA
+//Usuario0
+let user = {
+    nombre: 'Paola',
+    apellido: 'Ortiz',
+    email: 'paola@company.ru',
+    direccion: {
+        municipio: 'Guatemala',
+        calle: 'Calle Principal'
+    },
+}
+
+//Usuario1
+let user1 = {
+    nombre: 'Danilo',
+    apellido: 'Rodriguez',
+    email: 'danilo@company.ru',
+    direccion: {
+        municipio: 'Guatemala',
+        calle: 'Calle Ancha'
+    },
+}
+
+//Usuario2
+let user2 = {
+    nombre: 'Pablo',
+    apellido: 'Perez',
+    email: 'pablo@company.ru',
+    direccion: {
+        municipio: 'Guatemala',
+        calle: 'Calle Angosta'
+    },
+}
+
+//Usuario3
+let user3 = {
+    nombre: 'Andrea',
+    apellido: 'González',
+    email: 'andrea@company.ru',
+    direccion: {
+        municipio: 'Guatemala',
+        calle: 'Última calle'
+    },
+}
+
+//Usuario4
+let user4 = {
+    nombre: 'Marta',
+    apellido: 'Pérez',
+    email: 'marta@company.ru',
+    direccion: {
+        municipio: 'Guatemala',
+        calle: 'Calle Boulevard'
+    },
+}
+
+//Usuario5
+let user5 = {
+    nombre: 'Daniela',
+    apellido: 'Méndez',
+    email: 'daniela@company.ru',
+    direccion: {
+        municipio: 'Guatemala',
+        calle: 'Calle Fondo'
+    },
+}
