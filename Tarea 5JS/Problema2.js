ed7bb3b3a1313d8f2c6b0cdbb702d8ec3bc7ded7
Samira Barrera <164458958+samirabarrera@users.noreply.github.com>
2024-05-29 19:23:54 -0600

    Update Problema2.js

---
+++ Tarea 5JS/Problema2.js
@@ -0,0 +1,92 @@
+/*Crea otro código utilizando las características de los usuarios del
+Problema I, pero en esta ocasión agrega una funcionalidad, método
+recuperarClave*/
+
+let user = {
+    nombre: 'Paola',
+    apellido: 'Ortiz',
+    email: 'paola@company.ru',
+    direccion: {
+        municipio: 'Guatemala',
+        calle: 'Calle Principal'
+    },
+    estado: true,
+    recuperarClave: function () {
+        console.log('Clave recuperada exitosamente')
+    }
+}
+
+//Usuario1
+let user1 = {
+    nombre: 'Danilo',
+    apellido: 'Rodriguez',
+    email: 'danilo@company.ru',
+    direccion: {
+        municipio: 'Guatemala',
+        calle: 'Calle Ancha'
+    },
+    estado: true,
+    recuperarClave: function () {
+        console.log('Clave recuperada exitosamente')
+    }
+}
+
+//Usuario2
+let user2 = {
+    nombre: 'Pablo',
+    apellido: 'Perez',
+    email: 'pablo@company.ru',
+    direccion: {
+        municipio: 'Guatemala',
+        calle: 'Calle Angosta'
+    },
+    estado: true,
+    recuperarClave: function () {
+        console.log('Clave recuperada exitosamente')
+    }
+}
+
+//Usuario3
+let user3 = {
+    nombre: 'Andrea',
+    apellido: 'González',
+    email: 'andrea@company.ru',
+    direccion: {
+        municipio: 'Guatemala',
+        calle: 'Última calle'
+    },
+    estado: true,
+    recuperarClave: function () {
+        console.log('Clave recuperada exitosamente')
+    }
+}
+
+//Usuario4
+let user4 = {
+    nombre: 'Marta',
+    apellido: 'Pérez',
+    email: 'marta@company.ru',
+    direccion: {
+        municipio: 'Guatemala',
+        calle: 'Calle Boulevard'
+    },
+    estado: true,
+    recuperarClave: function () {
+        console.log('Clave recuperada exitosamente')
+    }
+}
+
+//Usuario5
+let user5 = {
+    nombre: 'Daniela',
+    apellido: 'Méndez',
+    email: 'daniela@company.ru',
+    direccion: {
+        municipio: 'Guatemala',
+        calle: 'Calle Fondo'
+    },
+    estado: true,
+    recuperarClave: function () {
+        console.log('Clave recuperada exitosamente')
+    }
+}
