55a858f70396142a60d952513da63920d3492790
Samira Barrera <164458958+samirabarrera@users.noreply.github.com>
2024-05-29 19:24:21 -0600

    Update Problema3.js

---
+++ Tarea 5JS/Problema3.js
@@ -0,0 +1,74 @@
+/*Crea otro código utilizando el Problema II, pero ahora agrega al
+objeto la propiedad dpi y el método cambiarDireccion.*/
+
+//Usuario0
+const user = { id: 1, dpi: 1111, };  
+user.name = 'Paola';
+console.log('Soy el objeto user, con la nueva propiedad', user);
+
+user.cambiarDireccion = function () { 
+    console.log('Cambiando su dirección ', user.name);
+}
+user.cambiarDireccion();
+console.log(user);
+console.log('Ahora cambiaremos la dirección');
+
+//Usuario1
+const user1 = { id: 2, dpi: 2222, };  
+user.name = 'Danilo';
+console.log('Soy el objeto user, con la nueva propiedad', user);
+
+user.cambiarDireccion = function () { 
+    console.log('Cambiando su dirección ', user.name);
+}
+user.cambiarDireccion();
+console.log(user);
+console.log('Ahora cambiaremos la dirección');
+
+//Usuario2
+const user2 = { id: 3, dpi: 3333, };  
+user.name = 'Pablo';
+console.log('Soy el objeto user, con la nueva propiedad', user);
+
+user.cambiarDireccion = function () { 
+    console.log('Cambiando su dirección ', user.name);
+}
+user.cambiarDireccion();
+console.log(user);
+console.log('Ahora cambiaremos la dirección');
+
+//Usuario3
+const user3 = { id: 4, dpi: 4444, };  
+user.name = 'Andrea';
+console.log('Soy el objeto user, con la nueva propiedad', user);
+
+user.cambiarDireccion = function () { 
+    console.log('Cambiando su dirección ', user.name);
+}
+user.cambiarDireccion();
+console.log(user);
+console.log('Ahora cambiaremos la dirección');
+
+//Usuario4
+const user4 = { id: 5, dpi: 5555, };  
+user.name = 'Marta';
+console.log('Soy el objeto user, con la nueva propiedad', user);
+
+user.cambiarDireccion = function () { 
+    console.log('Cambiando su dirección ', user.name);
+}
+user.cambiarDireccion();
+console.log(user);
+console.log('Ahora cambiaremos la dirección');
+
+//Usuario 5
+const user5 = { id: 6, dpi: 66666, };  
+user.name = 'Daniela';
+console.log('Soy el objeto user, con la nueva propiedad', user);
+
+user.cambiarDireccion = function () { 
+    console.log('Cambiando su dirección ', user.name);
+}
+user.cambiarDireccion();
+console.log(user);
+console.log('Ahora cambiaremos la dirección');
